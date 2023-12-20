eba6e75c55eeada8bcad22add526018e812f4446
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-12-21 01:52:04 +0500

    added static

--- static/home/js/main.js
+++ static/home/js/main.js
@@ -1,24 +1,23 @@
-const input = document.querySelector(".input");
-const output = document.querySelector(".output");
-const submit = document.querySelector(".submit");
-const from_language = document.querySelector("#from-language");
-const to_language = document.querySelector("#to-language");
 
-submit.addEventListener("click", () => {
-  const data = {
-    input: input.value.trim(),
-    from: from_language.value,
-    to: to_language.value,
-  };
-  submit.disabled = true;
-  axios
-    .post("translate/", data)
-    .then((response) => {
-      submit.disabled = false;
-      output.value = response.data.output;
-    })
-    .catch((error) => {
-      alert(error.response.data.error);
-      submit.disabled = false;
-    });
-});
+$('#sumbit-tels').on('click', event => {
+const from = $('#from-language').val()
+const to = $('#to-language').val()
+const text = $('.input').val()
+csrf_token = $('#csrf').val()
+console.log(from, to, text, csrf_token);
+
+$.ajax({
+    url: '/translate/',
+    type: 'POST',
+    data: {
+        from: from,
+        to: to,
+        text: text,
+        csrfmiddlewaretoken: csrf_token
+    },
+    success: function (data) {
+        $('.output').val(data.output)
+    }
+})
+
+})
--- translator/urls.py
+++ translator/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
 ]
-if settings.DEBUG:
+# if settings.DEBUG:
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     
     
